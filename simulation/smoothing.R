1fab5cf1d00c921150eef4db4659edeb66ceba0b
Long Qu <long.qu@wright.edu>
2015-07-10 18:14:04 -0400

    fixed up Wand & Ormerod binomality examples

--- simulation/smoothing.R
+++ simulation/smoothing.R
@@ -1,13 +1,14 @@
 library(gmp); library(bigPolyRat); library(varComp)
-library(splines); library(fda); library(nlme); library(lmeSplines)
+library(splines); library(fda); library(nlme);
 
 x0=-10:110 
 x=0:100
 n=length(x)
-knots = seq(0,100,length=25)
+knots = seq(0,100,length=27)
+int.knots=knots[seq(25)+1L]
 nknots=length(knots)-2
 X=cbind(1,x)
-brks = x
+int.x = 1:99 = x
 
 p=poly(x0, degree=18)[x0%in%x,]
     p = p*8 ## magic that makes the plots similar to those in the paper
@@ -24,19 +25,31 @@ y1=f1+err; y3=f2+err; y3=f3+err; y4=f2+sqrt(20)*err
 
 op=par(mfrow=c(2,2)); plot(x,y1);plot(x,y3);plot(x,y3);plot(x,y4);par(op)
 
+if(TRUE){
+    B.cs = bs(x, knots=1:99, intercept=TRUE)
+    Omega.co=bsplinepen(create.bspline.basis(rangeval = range(x), breaks=x, norder=4), Lfdobj = 2)
+    source("simulation/formOmega.R")
+    range(Omega.co-formOmega(0,100,1:99))
+    eig.O.cs=eigen(Omega.co, TRUE)
+    X.cs=B.cs%*%eig.O.cs$vec[,-seq(n)] ## this space does not include intercept. Why??
+    Z.cs=B.cs%*%sweep(eig.O.cs$vec[,seq(n)], 2, sqrt(eig.O.cs$val[seq(n)]), '/')
+    
+}else{
+    library(lmeSplines)  ## this has rank n-2 Z matrix rather than Z?
+    cs = smspline.v(x)
+    X.cs = cs$Xs
+    Z.cs = cs$Zs
+    K.cs = tcrossprod(Z.cs)
+    Z.cs.normalized = cholRoot(normalizeTrace(K.cs))
+    eig.K.cs=eigen(K.cs, TRUE)
+}
 
-cs = smspline.v(x)
-X.cs = cs$Xs
-Z.cs = cs$Zs
-K.cs = tcrossprod(Z.cs)
-Z.cs.normalized = cholRoot(normalizeTrace(K.cs))
-
-if(FALSE){
+if(TRUE){
 	B.co=bsplineS(x, breaks=knots, norder=4, nderiv=0, returnMatrix=FALSE)
 	Omega.co=bsplinepen(create.bspline.basis(rangeval = range(knots), breaks=knots, norder=4), Lfdobj = 2)
 	eig.O.co=eigen(Omega.co, symmetric = TRUE)
 	X.co = B.co%*%eig.O.co$vectors[,-seq(nknots+2)]
-	Z.co = B.co%*%eig.O.co$vectors[,seq(nknots+2)]/sqrt(eig.O.co$values[seq(nknots+2)])
+	Z.co = B.co%*%sweep(eig.O.co$vectors[,seq(nknots+2)], 2, sqrt(eig.O.co$values[seq(nknots+2)]),'/')
 	K.co=tcrossprod(Z.co)
 }else{
 	X.co=X
@@ -61,7 +74,7 @@ op=par(mfrow=c(1,2)); curve(fit1.co.preml(x), 0, .1);curve(fit1.co.preml(x), .1,
 (fit2.cs=varComp(y2~x, varcov=list(K.cs), control=list(start=1e4, plot.it=TRUE)))
 (fit2.co=varComp(y2~x, varcov=list(K.co), control=list(start=1e-3, plot.it=TRUE)))
 (fit2.co=varComp(y2~x, varcov=list(K.co), control=list(start=1e4, plot.it=TRUE)))
-#(fit2.cs.po=varComp(y2~x, varcov=list(K.cs), control=varComp.control('po', plot.it=TRUE))) #decartes=2
+#(fit2.cs.po=varComp(y2~x, varcov=list(K.cs), control=varComp.control('po', plot.it=TRUE))) #decartes=3
 (fit2.co.po=varComp(y2~x, varcov=list(K.co), control=list('po', plot.it=TRUE)))
 coef(fit2.co.po,'var.ratio')
 fit2.co.preml=Vectorize(logLik(fit2.co.po,FUN=T))
@@ -85,5 +98,7 @@ op=par(mfrow=c(1,2)); curve(fit3.co.preml(x), 0, 10);curve(fit3.co.preml(x), 10,
 #(fit4.cs.po=varComp(y4~x, varcov=list(K.cs), control=list('po',plot.it=TRUE)))
 (fit4.co.po=varComp(y4~x, varcov=list(K.co), control=list('po',plot.it=TRUE)))
 coef(fit4.co.po,'var.ratio')
+fit4.co.preml=Vectorize(logLik(fit4.co.po,FUN=T))
+op=par(mfrow=c(1,2)); curve(fit4.co.preml(x), 0, 5);curve(fit4.co.preml(x), 5, 2*coef(fit4.co.po,'tau')); par(op)
 
 save.image('smoothing.RData')
